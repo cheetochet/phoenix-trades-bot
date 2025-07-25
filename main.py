625f73696a81119e38601da70e122a7a329a2398
cheetochet <163370031+cheetochet@users.noreply.github.com>
2025-07-25 00:10:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,51 @@
 from flask import Flask, jsonify
 import os
+import json 
+import websocket
 
 app = Flask(__name__)
 
+def send_json_request(ws, req):
+    ws.send(json.dumps(req))
+
+def receive_json_response(ws):
+    response = ws.recv()
+    if response:
+        return json.loads(response)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app chetan🚅"})
+    ws = websocket.Websocket()
+    ws.connect("wss://gateway.discord.gg/?v=6&encording=json")
+    heartbeat_interval = receive_json_response(ws)["d"]["heartbeat_interval"]
+
+    token = "NzgxMzU3ODM5NjA4MTg0ODcy.Gcjxee.RThNACg3y3T-zVTyVtImZCjJePHO5imFwLGOJM"
+    payload = {
+        "op": 2,
+        "d": {
+            "token": token,
+            "intents": 513,
+            "properties": {
+                "$os": "linux",
+                "browser": "chrome", 
+                "device": "pc"
+            }
+        }
+    }
+    send_json_request(ws, payload)
+
+    count = 0
+    while True:
+        count++
+        if count > 10:
+            break
+        event=receive_json_response(ws)
+        try:
+            yield jsonify(event)
+        except:
+            break
+        
+    yield jsonify({"Choo Choo": "Welcome to your Flask app chetan🚅"})
 
 
 if __name__ == '__main__':
